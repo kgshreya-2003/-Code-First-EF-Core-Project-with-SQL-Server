f48696f714be14654b2672a22eb7b74e76c7e567
K G Shreya <shreya.kodiya@solugenix.com>
2025-09-17 15:09:56 +0530

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BookLibrary.Models;  // Assuming your Models are under this namespace
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibrary.Data  // Add a namespace for better organization
+{
+    public class AppDbContext : DbContext
+    {
+        // DbSets represent tables in your database
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<BorrowRecord> BorrowRecords { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BookLibraryDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;       
+using BookLibrary.Models;
+using BookLibrary.Data;                    
+
+class Program
+{
+    static void Main()
+    {
+        using var context = new AppDbContext();
+
+        if (!context.Books.Any())
+        {
+            var user = new User { FirstName = "John" };
+            var book = new Book { Title = "The Alchemist", ISBN = "9780061122415" };
+
+            context.Users.Add(user);
+            context.Books.Add(book);
+            context.SaveChanges();
+
+            var borrow = new BorrowRecord
+            {
+                BookId = book.BookId,
+                UserId = user.UserId,
+                BorrowDate = DateTime.Now
+            };
+
+            context.BorrowRecords.Add(borrow);
+            context.SaveChanges();
+
+            Console.WriteLine("Data seeded.");
+        }
+
+        
+        var borrowedBooks = context.BorrowRecords
+            .Include(br => br.Book)
+            .Include(br => br.User)
+            .Select(br => new
+            {
+                br.Book.Title,
+                br.Book.ISBN,
+                br.User.FirstName,
+                br.BorrowDate
+            })
+            .ToList();
+
+        foreach (var item in borrowedBooks)
+        {
+            Console.WriteLine($"{item.FirstName} borrowed '{item.Title}' (ISBN: {item.ISBN}) on {item.BorrowDate}");
+        }
+    }
+}
